f31a5bf7df3eee4362758905ae9c0392c6966ad6
Suhasich <suha.nri123@gmail.com>
2025-07-24 18:24:51 +0530

    My Name Is:

---
+++ src/suha/det/MobilePurchase.java
@@ -0,0 +1,16 @@
+package suha.det;
+
+public class MobilePurchase {
+	public void suhasiniCh() {
+		System.out.println("StudentOne");
+	}
+	public void chinnaSwamy() {
+		System.out.println("studenttwo");
+		
+}
+	public static void main(String[] args) {
+		BatchOne b= new BatchOne();
+		b.suhasiniCh();
+		b.chinnaSwamy();
+
+}
